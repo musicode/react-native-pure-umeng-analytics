6d496d59ba7a7771f63660c3579d8c7c34a84a2a
musicode <musicode@qq.com>
2020-03-27 15:17:48 +0800

    bump 0.0.7

--- android/src/main/java/com/github/musicode/umenganalytics/RNTUmengAnalyticsPackage.kt
+++ android/src/main/java/com/github/musicode/umenganalytics/RNTUmengAnalyticsPackage.kt
@@ -1,7 +1,5 @@
 package com.github.musicode.umenganalytics
 
-import java.util.Arrays
-
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -10,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 class RNTUmengAnalyticsPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return Arrays.asList<NativeModule>(RNTUmengAnalyticsModule(reactContext))
+        return listOf<NativeModule>(RNTUmengAnalyticsModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
